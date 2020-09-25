419137186b446b47506608d343e213657a1e940e
Kamzelski Bartosz <brk@pentacomp.pl>
2020-09-25 15:29:36 +0200

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/contacts', function () {
+    return view('contacts');
+})->name('contacts');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/calculations', function () {
+    return view('calculations');
+})->name('calculations');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/invoices', function () {
+    return view('invoices');
+})->name('invoices');
+
+Route::middleware(['auth:sanctum', 'verified'])->get('/settings', function () {
+    return view('settings');
+})->name('settings');
